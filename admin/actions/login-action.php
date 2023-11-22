543d79214609657dbac707f0085491d12a6e0d20
Dinesh Baidya <maildinesh@webgrity.net>
2023-11-22 19:14:29 +0530

    login backend

---
+++ admin/actions/login-action.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include '../../configuration.php';
+
+$userName = $_POST['username'];
+$password = $_POST['password'];
+
+if ($userName == '' || $password == '') {
+    $_SESSION['error_message'] = 'Please fill all the mendetory feild';
+    header('Location: ../login.php');
+    exit();
+}
--- admin/head.php
+++ admin/head.php
@@ -11,8 +11,7 @@
     <!-- custom style -->
     <script src="assets/js/script.js"></script>
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
